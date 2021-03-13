a452cd649070bfe2b5b5ff0da819e98ccbb64bf9
Wenfeng Zhu <wenfeng.zhu@campus.lmu.de>
2021-03-13 10:33:26 +0100

    complete the scrollbar of images

--- meme-generator/src/Components/Edit/Edit.js
+++ meme-generator/src/Components/Edit/Edit.js
@@ -1,44 +1,11 @@
-import React, {useState, useEffect, useReducer,useRef} from 'react';
+import React, {useState, useEffect, useReducer, useRef} from 'react';
 import './Edit.css'
-
-// const initState = {
-//     isLoaded: false,
-//     memesList: loadImages(),
-//     index: 0,
-// }
-//
-// function loadImages() {
-//     const memesList = [];
-//     fetch("https://api.imgflip.com/get_memes")
-//         .then(response => response.json())
-//         .then(result => {
-//             for (let i = 0; i < result['data']['memes'].length; i++) {
-//                 memesList[i] = result['data']['memes'][i];
-//             }
-//             return memesList;
-//         })
-// }
-//
-
-// function loadImagesReducer(state, action) {
-//     switch (action.type) {
-//         case 'pre':
-//             return {index: state.index === 0 ? memesList.length - 1 : state.index - 1};
-//         case 'next':
-//             return {index: state.index === (memesList.length - 1) ? 0 : state.index + 1};
-//         default :
-//             throw new Error();
-//     }
-// }
-
 
 function Edit() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [memesList, setMemes] = useState([]);
-    //const initialState = {index: 0};
-    // const [state,dispatch] = useReducer(loadImagesReducer,initialState);
-    const [index, setIndex] = useState(0);
+    const [currentIndex, setIndex] = useState(0);
 
     useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
@@ -51,10 +18,11 @@ function Edit() {
                 setError(error);
             });
 
+
     }, []);
 
-    const preImage = () => setIndex(index === 0 ? memesList.length - 1 : index - 1);
-    const nextImage = () => setIndex(index === memesList.length - 1 ? 0 : index + 1);
+    const preImage = () => setIndex(currentIndex === 0 ? memesList.length - 1 : currentIndex - 1);
+    const nextImage = () => setIndex(currentIndex === memesList.length - 1 ? 0 : currentIndex + 1);
 
 
     if (error) {
@@ -66,10 +34,24 @@ function Edit() {
             <div className="EditArea">
                 <div className="imageArea">
                     <div className="scrollBar">
-                        This is ScrollBar
+                        {/*The length of memesList is{memesList.length};*/}
+                        <p className="imageTitle">
+                            {memesList[currentIndex].name}
+                        </p>
+                        <div className="scrollImages" >
+                            {memesList.map((item, index) => {
+                                return (
+                                    <img key={index} className="scrollSingleImage" src={item.url}
+                                         alt="Image can not be displayed"
+                                         onClick={() => setIndex(index)}
+                                    />
+                                )
+                            })}
+                        </div>
+
                     </div>
-                    <div className="displayArea" >
-                        <img className="image" src={memesList[index].url} alt="Image can not be displayed" />
+                    <div className="displayArea">
+                        <img className="image" src={memesList[currentIndex].url} alt="Image can not be displayed"/>
                     </div>
                     <div className="buttonsArea">
                         <button className="preButton" onClick={preImage}>Pre</button>
